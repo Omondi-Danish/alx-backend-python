aeacb2783a85e29efed4de7a6c62d08d0d0ed23a
Omondi Danish <omondidanish77@gmail.com>
2025-09-07 21:56:11 +0300

    Create 1-batch_processing.py

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="yourpassword",
+            database="ALX_prodev"
+        )
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+
+        batch = []
+        for row in cursor:
+            batch.append(row)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+
+        if batch:  # yield remaining rows
+            yield batch
+
+        cursor.close()
+        connection.close()
+
+    except mysql.connector.Error as err:
+        print(f"Database error: {err}")
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                yield user
